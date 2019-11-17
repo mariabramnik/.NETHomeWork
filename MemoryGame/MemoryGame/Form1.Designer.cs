e70657140c15fd65d25f95ca5313cfecb1cecdfe
mariabramnik <37800336+mariabramnik@users.noreply.github.com>
2019-11-17 21:43:58 +0200

    Add files via upload

---
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "6",
+            "8"});
+            this.comboBox1.Location = new System.Drawing.Point(351, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(115, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox1.Location = new System.Drawing.Point(603, 333);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 84);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(740, 333);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 84);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 10F);
+            this.label1.Location = new System.Drawing.Point(63, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "please enter size of field";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(554, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "My Step blue";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(694, 421);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "My Step red";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+       
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MemoryGame/MemoryGame/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {
+        bool flagPlayer = true;
+        bool flag = false;
+        int size = 0;
+        Button[,] buttonArr;
+        List<int> list;
+        List<int> randomList = new List<int>();
+        int x = 0;
+        private static System.Timers.Timer aTimer;
+        private static System.Timers.Timer aTimer1;
+        private static System.Timers.Timer aTimer2;
+        string btnPress = "";
+        Button btmp;
+        Button btmp1;
+        Button bprev;
+        bool result = false;
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            // Check that the user entered the field size correctly;
+            #region
+            string str = comboBox1.SelectedItem.ToString();
+            int num = Int32.Parse(str);
+            size = num;
+            int count = 0;
+            #endregion
+            //  Clean and prepare the form for a new game;
+            #region
+            Control[] arr = new Control[this.Controls.Count];
+            Button b = new Button();
+            for (int i = 0; i < this.Controls.Count; i++)
+            {
+                arr[i] = Controls[i];
+            }
+            foreach (var contr in arr)
+            {
+                if (contr.GetType() == b.GetType())
+                {
+                    contr.Dispose();
+                }
+            }
+
+           pictureBox1.Top = 271;
+           pictureBox1.Height = 68;
+           pictureBox2.Top = 271;
+           pictureBox2.Height = 68;
+            #endregion
+           //fill the list with pairs of numbers
+            MatrixFilling();
+            //dynamically create a game field from buttons with text in form of numbers
+            #region
+            buttonArr = new Button[num, num];
+            
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    count++;
+                    string countStr = count.ToString();
+                    Button button = new Button();
+                    button.Name = countStr;
+                    button.Text = randomList[x].ToString();
+                    x++;                 
+                    button.Size = new System.Drawing.Size(37, 37);
+                    button.Left = (1 + button.Width) * i;
+                    button.Top = (1 + button.Height) * j + 60;
+                    button.Click += MyMethod;
+                    this.Controls.Add(button);
+                    buttonArr[i, j] = button;
+                   
+
+                }
+                
+            }
+            #endregion
+            // Create a timer with a 1.5 second interval.
+            aTimer = new System.Timers.Timer(1500);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+
+
+        }
+        //fill the list randomly with pairs of numbers
+        #region
+        private void  MatrixFilling()
+        {
+          //how many pairs of numbers do we need
+            int numbOfDigits = size * size / 2;
+            int[] arr = new int[size * size];
+             for (int i = 0; i < numbOfDigits;i++)
+            {
+                arr[i] = i + 1;
+                arr[i + numbOfDigits] = arr[i];
+            }
+            list = arr.ToList();
+            while(list.Count > 0)
+            {
+                int res = RandomFilling(list);
+                randomList.Add(res);
+
+            }
+
+        }
+        //randomly remove the numbers from list 
+        private int RandomFilling(List<int>list)
+        {
+            var res = list[rand.Next(list.Count)];
+            list.Remove(res);
+            return res;
+        }
+        #endregion
+        //at the beginning of the game after 1.5.seconds hide the text from the button
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            foreach (Button butt in buttonArr)
+            {
+                butt.ForeColor = System.Drawing.Color.Bisque;
+
+            }
+        }
+        //if the player has not guessed, then briefly show the text of the current button, 
+        //and then hide again
+        private void OnTimedEvent2(Object source, ElapsedEventArgs e)
+        {
+
+            
+            btmp.ForeColor = System.Drawing.Color.Bisque;
+
+            
+        }
+        //if the player has not guessed, then briefly show the text of the previous button, 
+        //and then hide again
+        private void OnTimedEvent3(Object source, ElapsedEventArgs e)
+        {
+
+
+            btmp1.ForeColor = System.Drawing.Color.Bisque;
+
+
+        }
+        //if the player has not guessed, then briefly show all the text, 
+        //and then hide again
+        private void OnTimedEvent4(Object source, ElapsedEventArgs e)
+        {
+            foreach (Button butt in buttonArr)
+            {
+                if (butt.Enabled != false)
+                {
+                    butt.ForeColor = System.Drawing.Color.Bisque;
+                }
+
+            }
+        }
+        //By Click the button
+        private void MyMethod(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            // flag shows even or odd step
+            if(flag == false)
+            {
+                flag = true;
+            }
+            else
+            {
+                flag = false;
+            }
+            //flagPlayer shows whose step
+            if(flagPlayer == true)
+            {
+                label3.Visible = false;
+                label2.Visible = true;
+            }
+            else
+            {
+                label2.Visible = false;
+                label3.Visible = true;
+            }
+            //if guessed
+            //the button cannot be pressed and the figure of the player(pictureBox) is growing
+            if(btnPress != "" && bt.Text == btnPress)
+            {
+                bt.Enabled = false;
+                bprev.Enabled = false;
+                if (flagPlayer == true)
+                {
+                    pictureBox1.Height = pictureBox1.Height + 20;
+                    pictureBox1.Top = pictureBox1.Top - 20;
+                    
+                }
+                else
+                {
+                    pictureBox2.Height = pictureBox2.Height + 20;
+                    pictureBox2.Top = pictureBox2.Top - 20;
+                   
+                }
+              
+                    btnPress = "";
+                  
+                
+ 
+            }
+            //if not guessed
+            if(btnPress != "" && bt.Text != btnPress)
+            {
+                TextAppear(bt);
+                HideText(bprev);
+                // step moves to another player
+                if (flagPlayer == true)
+                {
+                    flagPlayer = false;
+                }
+                else
+                {
+                    flagPlayer = true;
+                }
+                
+                btnPress = "";
+                bprev = null;
+                AllMatrixAppear();
+                
+            }
+            if (flag == true)
+            {
+                btnPress = bt.Text;
+                bprev = bt;
+  
+            }
+
+            //check if the game is over
+            result = CheckEndGame();
+            if(result == true)
+            {
+                if(pictureBox1.Top < pictureBox2.Top)
+                {
+                    MessageBox.Show("End Of The Game !!!The Blue won the game!!!");
+                    
+                }
+                else
+                {
+                    MessageBox.Show("End Of The Game !!!The Rad won the game!!!");
+                }
+            }
+        }
+        //refresh the button text in our memory
+        public void TextAppear(Button btn)
+        {
+            btmp = btn;
+            btmp.ForeColor = System.Drawing.Color.Black;
+            // Create a timer with a one second interval.
+            aTimer = new System.Timers.Timer(1000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent2;
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+        }
+        public void HideText(Button btn)
+        {
+            btmp1 = btn;
+            btmp1.ForeColor = System.Drawing.Color.Black;
+            // Create a timer with a one second interval.
+            aTimer1 = new System.Timers.Timer(800);
+            // Hook up the Elapsed event for the timer. 
+            aTimer1.Elapsed += OnTimedEvent3;
+            aTimer1.AutoReset = false;
+            aTimer1.Enabled = true;
+        }
+        public bool CheckEndGame()
+        {
+            bool res = true;
+            foreach (Button butt in buttonArr)
+            {
+                if (butt.Enabled != false)
+                {
+                    res = false;
+                }
+
+            }
+            return res;
+        }
+        //refresh the all buttons text in our memory
+        public void AllMatrixAppear()
+        {
+            foreach (Button butt in buttonArr)
+            {
+
+                    butt.ForeColor = System.Drawing.Color.Black;
+ 
+
+            }
+           
+            // Create a timer with a 1.5 second interval.
+            aTimer2 = new System.Timers.Timer(1500);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent4;
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+        }
+
+    }
+}
