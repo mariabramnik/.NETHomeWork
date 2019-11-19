0fcf8b3e2fd05921d6c78527ff4e151dfa023256
mariabramnik <37800336+mariabramnik@users.noreply.github.com>
2019-11-19 23:43:38 +0200

    Add files via upload

---
+++ SimonGame1/SimonGame1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SimonGame1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(279, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 119);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(424, 107);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(135, 119);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(279, 232);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(139, 134);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(424, 232);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(135, 134);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(12, 378);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(112, 91);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(326, 444);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 51);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 539);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SimonGame1/SimonGame1/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SimonGame1.Properties;
+using System.Media;
+using System.Threading;
+
+namespace SimonGame1
+{
+    public partial class Form1 : Form
+    {
+        //pictures initialization
+        #region
+        Image BtnBlue = Resources.bt1;
+        Image BtnGreen = Resources.bt2;
+        Image BtnOrange = Resources.bt3;
+        Image BtnRed = Resources.bt4;
+        Image BtnStart = Resources.btStart;
+        Image Gold = Resources.gold;
+        #endregion
+
+        int count = 0;
+        bool flag = false;
+        Random rand = new Random();
+        int[] arrCount = new int[4] { 1, 2, 3, 4 };
+        PictureBox[] pbArr;
+        PictureBox currentPictureBox = new PictureBox();
+        Image imageTmp;
+        Image imageGold;
+        SoundPlayer sp = new SoundPlayer();
+        int[] arrNumbers1;
+        int[] arrNumbers2;
+        List<int> listNumbers2 = new List<int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+         // insert background pictures into pictureBox elements
+          //pictures look like buttons
+            #region
+            pictureBox1.Image = BtnBlue;
+            pictureBox2.Image = BtnGreen;
+            pictureBox3.Image = BtnOrange;
+            pictureBox4.Image = BtnRed;
+            pictureBox5.Image = BtnStart;
+            imageGold = Gold;
+            #endregion
+            textBox1.Text = "HELLO";
+
+        }
+        //beginning of the game
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            pbArr = new PictureBox[4] { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
+            List<PictureBox> pbList = pbArr.ToList();
+            pictureBox5.Enabled = false;
+            Exercise();
+        }
+        //  Each time the melody is extended by one sound and one flashing button
+        public void Exercise()
+        {
+            Cursor = Cursors.WaitCursor;
+            Thread.Sleep(1000);
+            listNumbers2.Clear();
+            textBox1.Hide();
+            count++;
+            arrNumbers1 = new int[count];
+            for(int i = 0; i < count; i++)
+            {            
+               int number =  Blinking();
+                arrNumbers1[i] = number;
+
+            }
+            // The flag=true indicates that it is now the player’s turn to
+            //repeat the melody.
+            flag = true;
+            Cursor = Cursors.Default;
+
+        }
+        //flashing buttons
+        public int Blinking()
+        {
+            int num = arrCount[rand.Next(arrCount.Length)];
+            currentPictureBox = pbArr[num - 1];
+            imageTmp = currentPictureBox.Image;
+            currentPictureBox.Image = Gold;
+            currentPictureBox.Update();
+            Thread.Sleep(100);
+            currentPictureBox.Image = imageTmp;
+            currentPictureBox.Update();
+            MySound(num);
+            Thread.Sleep(1000);
+            return num;           
+        }
+
+     // each button has a sound
+        public void MySound(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                sp.Stream = Properties.Resources._do;
+                break;          
+                case 2:
+                    sp.Stream = Resources.fa;
+                    break;
+                case 3:
+                    sp.Stream = Resources.lja;
+                    break;
+                case 4:
+                    sp.Stream = Resources.si;
+                    break;
+                default:
+                    sp.Stream = Properties.Resources._do;
+                    break;
+            }
+            sp.Play();
+        }
+        // when a player presses a button
+        #region
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MySound(1);
+            MyBlincking(pictureBox1);
+            listNumbers2.Add(1);
+            if (listNumbers2.Count == count)
+            {
+                Answer();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            MySound(2);
+            MyBlincking(pictureBox2);
+            listNumbers2.Add(2);
+            if (listNumbers2.Count == count)
+            {
+
+                Answer();
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            MyBlincking(pictureBox3);
+            MySound(3);
+            listNumbers2.Add(3);
+            if (listNumbers2.Count == count)
+            {
+                Answer();
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            MyBlincking(pictureBox4);
+            MySound(4);
+            listNumbers2.Add(4);
+            if (listNumbers2.Count == count)
+            {
+                Answer();
+            }
+        }
+        #endregion
+
+        // compare the player’s response with the original melody
+        public void Answer()
+        {
+            textBox1.Visible = true; 
+            bool res = true;
+            arrNumbers2 = listNumbers2.ToArray();
+            for(int i = 0; i < count; i++)
+            {
+                if (arrNumbers1[i] != arrNumbers2[i])
+                {
+                    res = false;
+                }
+            }
+           if(res == true)
+            {
+                textBox1.ForeColor = System.Drawing.Color.Red;
+                textBox1.Text = "YESSS!!!";
+                textBox1.Update();
+                Thread.Sleep(1000);
+                Exercise();
+            }
+            else
+            {
+                pictureBox5.Enabled = true;
+                textBox1.Text = "Oops!!";
+                count = 0;
+            }
+            flag = false;
+           
+  
+
+        }
+    //  flashing button when pressed by player
+        public void MyBlincking(PictureBox pb)
+        {
+           
+            imageTmp = pb.Image;
+            pb.Image = Gold;
+            pb.Update();
+            Thread.Sleep(100);
+            pb.Image = imageTmp;
+            pb.Update();
+
+
+        }
+        
+
+    }
+}
