af009fe7657eac8fb0d4c5fb60601305782228ce
mariabramnik <37800336+mariabramnik@users.noreply.github.com>
2019-12-19 16:16:59 +0200

    Add files via upload

---
+++ Camp22HomeWork/Camp.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Camp22HomeWork
+{
+    public class Camp
+    {
+        public  int Id { get;set; }
+        public int Latitude{ get;set; }
+        public int Logitude { get;  set; }
+        public int NumberOfPeople { get;  set; }
+        public int NumberOfTents { get;  set; }
+        public int NumberOfFleshLights { get; set; }
+        public static int lastCampId = 0;
+        public Camp(int latitude,int logitude,int numberOfPeople,
+            int numberOfTents,int numberOfFleshLights)
+        {
+            lastCampId++;
+            Latitude = latitude;
+            Logitude = logitude;
+            NumberOfPeople = numberOfPeople;
+            NumberOfTents = numberOfTents;
+            NumberOfFleshLights = numberOfFleshLights;
+            Id = lastCampId;
+        }
+        public Camp(int a, int b)    
+        {
+            lastCampId++;
+            Id = lastCampId;
+        }
+        public Camp()
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"Camp N={Id}, Camp coordinated Y={Latitude} , X={Logitude}," +
+                $"Number Of People in the camp = {NumberOfPeople} , Number of tents in the camp = " +
+                $"{NumberOfTents} , Number of Fleshlights = {NumberOfFleshLights}";
+        }
+        public static bool operator == (Camp c1, Camp c2)
+        {
+            if (c1.Id == c2.Id)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Camp c1, Camp c2)
+        {
+            if (ReferenceEquals(c1, null) && ReferenceEquals(c2, null))
+            {
+                return true;
+            }
+            if (ReferenceEquals(c1, null) || ReferenceEquals(c2, null))
+            {
+                return false;
+            }
+            return c1.Id == c2.Id;
+        }
+        public static bool operator >(Camp c1, Camp c2)
+        {
+            if (c1 == c2)
+            {
+                return false;
+            }
+            if (ReferenceEquals(c1, null) || ReferenceEquals(c2, null))
+            {
+                return false;
+            }
+            return c1.NumberOfPeople > c2.NumberOfPeople;
+        }
+        public static bool operator <(Camp c1, Camp c2)
+        {
+            if (c1 == c2)
+            {
+                return false;
+            }
+            if (ReferenceEquals(c1, null) || ReferenceEquals(c2, null))
+            {
+                return false;
+            }
+            return c1.NumberOfPeople < c2.NumberOfPeople;
+        }
+        public override bool Equals(object obj)
+        {
+            Camp camp2 = (Camp)obj;
+            if(camp2.Id == Id)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            
+            return this.Id;
+        }
+        public static Camp operator +(Camp c1,Camp c2)
+        {
+            Camp c3 = new Camp();
+            c3.NumberOfPeople = c1.NumberOfPeople + c2.NumberOfPeople;
+            c3.NumberOfTents = c1.NumberOfTents + c2.NumberOfTents;
+            c3.NumberOfFleshLights = c1.NumberOfFleshLights + c2.NumberOfFleshLights;
+            lastCampId++;
+            c3.Id = lastCampId;
+            return c3;
+        }
+        public void SerializeXmlToFaile(string fileName)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(Camp));
+            string path = @"C:\test\" + fileName;
+            using (FileStream fs = new FileStream(path,FileMode.OpenOrCreate))
+            {
+                ser.Serialize(fs, this);
+                Console.WriteLine("write to Xml -file successful");
+            }
+        }
+        public static Camp DeSerializeXmlFaileToCamp(string fileName)
+        {
+            Camp c3 = new Camp();
+            XmlSerializer ser = new XmlSerializer(typeof(Camp));
+            string path = @"C:\test\" + fileName;
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                c3 = (Camp)ser.Deserialize(fs);
+                Console.WriteLine("read from Xml-file completed successfully");
+            }
+            return c3;
+        }
+        //just an example. how works json 
+        public void SerializeJsonToFaile(string fileName)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(this, options);
+            string path = @"C:/test/" + fileName;
+            //  string json = JsonSerializer.Serialize<Camp>(this);
+            //  Console.WriteLine(json);
+            File.WriteAllText(path, jsonString);
+            Console.WriteLine("write to Json-file successful");
+            
+        }
+        public static Camp DeSerializeJsonFaileToCamp(string fileName)
+        {
+           
+            string path = @"C:/test/" + fileName;
+            string stringJson = File.ReadAllText(path);
+            Console.WriteLine("json" + stringJson);
+           Camp c3 = JsonSerializer.Deserialize<Camp>(stringJson);
+            Console.WriteLine("read from Json-file completed successfully");
+            
+            return c3;
+        }
+    }
+}
---
+++ Camp22HomeWork/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camp22HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camp c1 = new Camp(123, 345, 10, 10, 10);
+            Camp c2 = new Camp(123, 345, 20, 20, 20);
+            if(c1 > c2)
+            {
+                Console.WriteLine("first camp is larger than the second");
+            }
+            else
+            {
+                Console.WriteLine("second camp is larger than the first");
+            }
+            if(c1 == c2)
+            {
+                Console.WriteLine("first camp is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("first camp is not equal to the second");
+            }
+            if(c1.Equals(c2))
+            {
+                Console.WriteLine("first camp is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("first camp is not equal to the second");
+            }
+            Console.WriteLine(c1);            
+            Camp c3 = c1 + c2;
+            c3.Latitude = 456;
+            c3.Logitude = 765;
+            Console.WriteLine(c3);
+            Console.WriteLine(c1);
+            c1.SerializeXmlToFaile("camp.xml");
+            Camp c4 = Camp.DeSerializeXmlFaileToCamp("camp.xml");
+            Console.WriteLine($"c1 HashCode = {c1.GetHashCode()}");
+            Console.WriteLine($"c4 HashCode = {c4.GetHashCode()}");
+            if (c1 == c4 && c1.GetHashCode() == c4.GetHashCode())
+            {
+                Console.WriteLine("Serializing and deserializing did not change the camp");
+            }
+            else
+            {
+                Console.WriteLine("Serializing and deserializing changed the camp");
+            }
+            c4.SerializeJsonToFaile("campJson.json");
+            Camp c6 = Camp.DeSerializeJsonFaileToCamp("campJson.json");
+            Console.WriteLine(c6);
+            Console.ReadLine();
+        }
+    }
+}
