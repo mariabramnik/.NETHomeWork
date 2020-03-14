d4cd86094c804e3a800e92beb3e35639cdc13969
mariabramnik <37800336+mariabramnik@users.noreply.github.com>
2020-03-14 17:48:38 +0200

    Add files via upload

---
+++ HomeWork49ComboBox/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeWork49ComboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Country[] countries = Country.GetCountries();
+            myComboBox.ItemsSource = countries;
+            myComboBox1.ItemsSource = countries;
+            this.myComboBox1.SelectionChanged += new SelectionChangedEventHandler(OnMyComboBoxChanged);
+
+
+        }
+
+        private void OnMyComboBoxChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //string text = this.myComboBox1.Text;
+            //MessageBox.Show(text);
+            myComboBox1.Background = Brushes.Red;
+
+
+
+
+        }
+    }
+}
---
+++ HomeWork49ComboBox/Model/Country.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork49ComboBox
+{
+   public class Country : INotifyPropertyChanged
+    {
+       public string Name { get; set; }
+       public string Capital { get; set; }
+       public string PathToImg { get; set; }
+
+        public Country()
+        {
+        }
+
+        public Country(string name, string capital, string pathToImg)
+        {
+            this.Name = name;
+            this.Capital = capital;
+            this.PathToImg = pathToImg;
+        }
+        public static Country[] GetCountries()
+        {
+            var result = new[] {
+               new  Country() {Name="Israel", Capital = "Jerusalim", PathToImg = "Resources/israel.png"},
+                new  Country() {Name="Latvia", Capital = "Riga" ,PathToImg="Resources/latvia.png"},
+                new  Country() {Name="Czech", Capital = "Praga", PathToImg="Resources/czech.png"}
+        };
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Capital}";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
