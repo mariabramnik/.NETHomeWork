1741623af8a9ebd0e197a317a00501317858eaec
mariabramnik <37800336+mariabramnik@users.noreply.github.com>
2020-06-22 15:26:04 +0300

    Add files via upload

--- ASPProject2/Controllers/AnonymeController.cs
+++ ASPProject2/Controllers/AnonymeController.cs
@@ -5,17 +5,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using StackExchange.Redis;
+using System.Text.Json;
+using System.Net;
+using System.Diagnostics;
 
 namespace ASPProject2
 {
     public class AnonymeController : ApiController
     {
-         AnonymousUserFacade anonymeFacade = new AnonymousUserFacade();
+        
+        AnonymousUserFacade anonymeFacade = new AnonymousUserFacade();
+        private ConnectionMultiplexer redis;
+        private static IDatabase dbRedis;
+
+        public AnonymeController()
+        {
+            redis = ConnectionMultiplexer.Connect("localhost,allowAdmin=true");
+            var endpoints = redis.GetEndPoints();
+            var server = redis.GetServer(endpoints[0]);
+            server.FlushAllDatabases();
+
+            dbRedis = redis.GetDatabase();
+            List<Flight>result = (List<Flight>)anonymeFacade.GetAllFlights();
+            foreach (Flight flight in result)
+            {
+                string redisvalueJson = JsonSerializer.Serialize<Flight>(flight);
+                dbRedis.StringSet(flight.id.ToString(),redisvalueJson);
+            }
+        }
+
 
         [HttpGet]
         [Route("api/anonyme/allflights")]
         public IHttpActionResult GetAllFlights()
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             List<Flight> result = new List<Flight>();
             try
             {
@@ -26,8 +52,39 @@ namespace ASPProject2
             {
                 return InternalServerError(e);
             }
+            stopwatch.Stop();
+            int millisek = (int)stopwatch.ElapsedMilliseconds;
             return Ok(result);
         }
+        [HttpGet]
+        [Route("api/anonyme/allflightsredis")]
+        public IHttpActionResult GetAllFlightsRedis()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            List<Flight> result = new List<Flight>();
+            try
+            {
+                
+                EndPoint endPoint = redis.GetEndPoints().First();
+                RedisKey[] keys = redis.GetServer(endPoint).Keys(pattern: "*").ToArray();
+                foreach (RedisKey key in keys)
+                {
+                    string value = dbRedis.StringGet(key);
+                    Flight restoredFlight = JsonSerializer.Deserialize<Flight>(value);
+                    result.Add(restoredFlight);
+                }
+  
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+            stopwatch.Stop();
+            int millisek = (int)stopwatch.ElapsedMilliseconds;
+            return Ok(result);
+        }
+
 
         [HttpGet]
         [Route("api/anonyme/allcountries")]
@@ -85,6 +142,8 @@ namespace ASPProject2
         [Route("api/anonyme/flight/{id}")]
         public IHttpActionResult GetFlightById(int id)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             Flight flight =  new Flight();
             try
             {
@@ -98,6 +157,33 @@ namespace ASPProject2
             {
                 return InternalServerError(e);
             }
+            stopwatch.Stop();
+            int millisek = (int)stopwatch.ElapsedMilliseconds;
+            return Ok(flight);
+        }
+
+        [HttpGet]
+        [Route("api/anonyme/flightredis/{id}")]
+        public IHttpActionResult GetFlightByIdRedis(int id)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Flight flight = new Flight();
+            try
+            {
+                string res = dbRedis.StringGet(id.ToString());
+                flight = JsonSerializer.Deserialize<Flight>(res);
+            }
+            catch (FlightNotExistException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+            stopwatch.Stop();
+            int millisek = (int)stopwatch.ElapsedMilliseconds;
             return Ok(flight);
         }
 
